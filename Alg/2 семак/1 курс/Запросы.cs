e487b2bfd58082129d04f0cdd4656099a5bcec55
Kuzina-Varya <145117807+Kuzina-Varya@users.noreply.github.com>
2024-05-23 15:15:09 +0600

    Create Запросы.cs

---
+++ Alg/2 семак/1 курс/Запросы.cs
@@ -0,0 +1,40 @@
+﻿namespace Запросы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 5, 2, 3, 48, 59, 61, 25, 58, 74, 71, 72, 35, 65, 99 };
+            List<int> list = new List<int>();
+            int value;
+            for (int i = 0; i < array.Length; i++) 
+            {
+                value = array[i];
+                while (array[i] != 0)
+                {
+                    if (array[i]%10%2==0)
+                    {
+                  
+                        list.Add(value);
+                        break;
+                    }
+                    else
+                    {
+                        
+                        array[i] /= 10;
+
+                    }
+                    
+                }
+            }
+            var linq = from a1 in list
+                       where a1 % 3 == 0
+                       select a1 ;
+            foreach (var a in linq)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
+
