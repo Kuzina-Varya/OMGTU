da4375b17dab3279b58894f33b050eecc547e054
Kuzina-Varya <145117807+Kuzina-Varya@users.noreply.github.com>
2024-06-11 15:38:08 +0600

    Create Золотая рыбка.cs

---
+++ Olim/2 семак/1 курс/Золотая рыбка.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var words = new HashSet<string>();
+        for (int i = 0; i < n; i++)
+        {
+            words.Add(Console.ReadLine());
+        }
+
+        int f = int.Parse(Console.ReadLine());
+        var firstLetters = new Dictionary<char, int>();
+        for (int i = 0; i < f; i++)
+        {
+            var parts = Console.ReadLine().Split(' ');
+            char c = parts[0][0];
+            int k = int.Parse(parts[1]);
+            firstLetters[c] = k;
+        }
+
+        int l = int.Parse(Console.ReadLine());
+        var lastLetters = new Dictionary<char, int>();
+        for (int i = 0; i < l; i++)
+        {
+            var parts = Console.ReadLine().Split(' ');
+            char c = parts[0][0];
+            int k = int.Parse(parts[1]);
+            lastLetters[c] = k;
+        }
+
+        int count = 0;
+        var usedWords = new HashSet<string>();
+        foreach (var word in words)
+        {
+            if (firstLetters.ContainsKey(word[0]) && lastLetters.ContainsKey(word[word.Length - 1]) && !usedWords.Contains(word))
+            {
+                int firstLetterCount = firstLetters[word[0]];
+                int lastLetterCount = lastLetters[word[word.Length - 1]];
+                int minCount = Math.Min(firstLetterCount, lastLetterCount);
+                count += minCount;
+                firstLetters[word[0]] -= minCount;
+                lastLetters[word[word.Length - 1]] -= minCount;
+                usedWords.Add(word);
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
